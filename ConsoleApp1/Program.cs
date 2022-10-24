361a1f65d4f5089daf1a6f202ead746faf5f907f
hatobanono <chris00iq@gmail.com>
2022-10-24 12:50:14 +0800

    complete

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,17 +10,24 @@ namespace ConsoleApp1
 	{
 		static void Main(string[] args)
 		{
-			for (int i = 1; i <=5; i++)
+			int rows = 5;
+			for (int i = 1; i <= rows; i++) 
 			{
-				string row1 = new string('+', 5 - i);
-				Console.Write(row1);
+				//string row1 = new string('+', 5 - i);
+				//Console.Write(row1);
 
-				for (int k = 1; k <= i; k++) 
-				{
-					Console.Write(i);
-				}
+				//for (int k = 1; k <= i; k++)
+				//{
+				//	Console.Write(i);
+				//}
 
-				Console.WriteLine();
+				//Console.WriteLine();
+
+				string plusString = new string('+', rows - i);
+				string numbers = new string(i.ToString()[0], i);
+				string row = plusString + numbers;
+				Console.WriteLine(row);
+
 
 			}
 			
